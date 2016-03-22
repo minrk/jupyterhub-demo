18a490249bb0cab6291153786b3fe81aab0df2bb
Min RK <benjaminrk@gmail.com>
2016-03-22 15:42:59 +0100

    update docker install command

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ apt-get install \
   npm nodejs-legacy \
   python3-dev python3-pip
 
-which -s docker || curl -SL https://get.docker.com/ubuntu/ | sh
+which -s docker || curl -sSL https://get.docker.com | sh
 
 pip3 install -r requirements.txt
 
