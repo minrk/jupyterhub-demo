bec4c64bd648e95b0f77ff31e869a942c25eca8e
Min RK <benjaminrk@gmail.com>
2017-10-04 12:54:18 +0200

    get rmate

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ apt-get install \
   npm nodejs-legacy \
   python3-dev python3-pip
 
+# get rmate for admin
+curl -sL  https://raw.githubusercontent.com/aurora/rmate/1423bf0773059209486bb808b5b0abea115339aa/rmate > /usr/local/bin/rmate
+chmod a+x /usr/local/bin/rmate
 which docker || curl -sSL https://get.docker.com | sh
 
 pip3 install -r requirements.txt
